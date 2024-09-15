8ebdad0d25e2abb9c6fef0bf0781755ab374993f
Allen Porter <allen.porter@gmail.com>
2024-09-15 18:27:48 +0000

    Fix test

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -61,7 +61,7 @@ journal are exposed as entities in the Home Assistant and are listed below.
         "results": [
             {
                 "content": "document",
-                "id": "098f6bcd4621d373cade4e832627b4f6",
+                "id": "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
                 "notebook": "Daily",
             },
         ],
